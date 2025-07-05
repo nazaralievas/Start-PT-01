c8c4d024103acfc8c3831c24d753655cafebef59
nazaralievas <sadanaza4@gmail.com>
2025-07-05 16:12:56 +0600

    todo added

---
+++ todo/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200))
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/', methods=['GET', 'POST'])
+def homepage():
+    if request.method == "POST":
+        text = request.form['text']
+        task = Todo(text=text)
+        db.session.add(task)
+        db.session.commit()
+        return redirect('/')
+    return render_template('homepage.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/rules')
+def rules():
+    return render_template('rules.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
