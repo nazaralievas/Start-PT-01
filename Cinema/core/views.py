a45abedc44f02faa090d27c003fadce10897f728
nazaralievas <sadanaza4@gmail.com>
2025-07-13 16:10:10 +0600

    movie detail added

--- Cinema/core/views.py
+++ Cinema/core/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render
 from .models import Movie
 
 def movie_list(request):
-    movies = Movie.objects.all()
+    q = request.GET.get('q')
+    if q:
+        movies = Movie.objects.filter(name__icontains=q)
+    else:
+        movies = Movie.objects.all()
     return render(request, 'core/movie_list.html', {'movies': movies})
+
+
+def movie_detail(request, id):
+    movie = Movie.objects.get(id=id)
+    return render(request, 'core/movie_detail.html', {'movie': movie})
